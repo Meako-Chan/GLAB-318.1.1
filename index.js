4a29952ca4c8c5970d46c31bcb9d20c8882d3a39
Michael Chan <michael.w.chan01@gmail.com>
2024-06-27 15:12:09 -0400

    Added some routes

--- index.js
+++ index.js
@@ -3,10 +3,23 @@ const hostname = '127.0.0.1';
 const port = 3000;
 const server = http.createServer((req, res) =>{
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<h1 style="color: red">Hello World!</h1>');
-    res.write('<p>I wonder what else we can send...</p>');
-    res.end();
+    var url = req.url;
+    if(url === '/'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<h1 style="color: red">Hello World!</h1>');
+        res.write('<p>I wonder what else we can send...</p>');
+        res.end();
+    }else if(url ==='/about'){
+        res.write('<h1> About me </h1>');
+        res.end();
+    }else if(url ==='/no'){
+        res.write('<h1 style="font-size: 500px"> NO </h1>');
+        res.end();
+    }else if(url ==='/time'){
+        res.write(new Date().toString());
+        res.end();
+    }
+    
 });
 
 server.listen(port, hostname, () => {
